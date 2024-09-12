bd2814137dcaac4e21909ceda4bb27d0932bc2a7
meemmahmuda <meemmahmuda85@gmail.com>
2024-09-12 06:34:27 +0600

    up

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -237,7 +237,7 @@ public function report(Request $request)
     return view('sales.report', [
         'reportData' => $reportData,
         'selectedDate' => $date,
-        'selectedMonth' => $
+        'selectedMonth' => $month
     ]);
 }
 
