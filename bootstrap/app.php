69ab7452e1c11d6ad12a1c3ea92a03aa9c36e679
meemmahmuda <meemmahmuda85@gmail.com>
2024-09-14 11:15:01 +0600

    up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::,
+    Illuminate\Contracts\Http\Kernel::class,
     App\Http\Kernel::class
 );
 
